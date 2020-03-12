efe3b1ada08722fc9ad08aed1e7ec934c2901476
Chris Holdgraf <choldgraf@berkeley.edu>
2020-03-12 12:10:07 -0700

    adding CLI

--- nbclean/__init__.py
+++ nbclean/__init__.py
@@ -4,4 +4,3 @@ __version__ = "0.3.2"
 
 from .clean import NotebookCleaner
 from .run import run_notebook_directory, run_notebook
-
--- nbclean/cli.py
+++ nbclean/cli.py
@@ -1,6 +1,7 @@
 import click
 import nbformat as nbf
 from glob import glob
+from nbclient import execute
 from .clean import NotebookCleaner
 from .run import run_notebook
 
@@ -10,22 +11,50 @@ def main():
     pass
 
 @main.command()
-@click.argument("pattern")
+@click.argument("path")
 @click.option("--kind", default="output", help="What element of the notebooks you wish to clear")
-def clear(pattern, kind="output", **kwargs):
-    notebooks = glob(pattern, recursive=True)
+@click.option("--skip", default=".ipynb_checkpoints", help="What element of the notebooks you wish to clear")
+def clear(path, kind, skip, **kwargs):
+    clear_notebooks(path, kind, skip=skip, **kwargs)
+
+def _find_notebooks(path, skip):
+    from pathlib import Path
+    path = Path(path)
+    if not path.exists():
+        raise ValueError(f"You gave a path that doesn't exist: {path}")
+    elif path.is_dir():
+        notebooks = list(path.rglob("*.ipynb"))
+        notebooks = [ii for ii in notebooks if skip not in str(ii)]
+    elif path.suffix == ".ipynb":
+        notebooks = [str(path)]
+    else:
+        raise ValueError(f"You gave a path that isn't a folder or a notebook file: {path}")
+    return notebooks
+
+def clear_notebooks(path, kind, skip=".ipynb_checkpoints", **kwargs):
+    notebooks = _find_notebooks(path, skip)
+    if len(notebooks) == 0:
+        print("Note: no notebooks were found to be cleared.")
     for path in notebooks:
-        cleaner = NotebookCleaner(path)
+        print(f"Clearing {kind} in {path}")
+        cleaner = NotebookCleaner(str(path))
         cleaner.clear(kind, **kwargs)
-        cleaner.save(path)
+        cleaner.save(str(path))
 
 
 @main.command()
-@click.argument("pattern")
-@click.option("--max_output_lines", default=1000, help="The maximum number of lines allowed in notebook outputs.")
-def run(pattern, max_output_lines=1000, **kwargs):
-    notebooks = glob(pattern, recursive=True)
+@click.argument("path")
+@click.option("--skip", default=".ipynb_checkpoints", help="What element of the notebooks you wish to clear")
+def run(path, skip, **kwargs):
+    run_notebooks(path, skip, **kwargs)
+
+def run_notebooks(path, skip, **kwargs):
+    notebooks = _find_notebooks(path, skip)
+    if len(notebooks) == 0:
+        print("Note: no notebooks were found to be executed.")
+
     for path in notebooks:
         print(f"Running {path}")
-        ntbk = run_notebook(path, max_output_lines=max_output_lines)
-        nbf.write(ntbk, path)
+        ntbk = nbf.read(str(path), nbf.NO_CONVERT)
+        ntbk = execute(ntbk, cwd=str(path.parent))
+        nbf.write(ntbk, str(path))
